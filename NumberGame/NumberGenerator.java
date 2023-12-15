4c6957319dc0552a67b42fb6a3a91bca46074fe1
Snomvuzo Dlamini <snomvuzo26@gmail.com>
2023-12-15 20:36:33 +0200

    compared generated number with user input

--- NumberGame/NumberGenerator.java
+++ NumberGame/NumberGenerator.java
@@ -4,37 +4,43 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class NumberGenerator {
-    private final Random random;
+    public static void main(String[] args) {
 
-    public NumberGenerator(){
-        this.random = new Random();
-    }
+//    public void generateNumber() {
+        Random random = new Random();
 
-    public void generateNumber(){
-        StringBuilder num = new StringBuilder("");
+        int num = 0;
         for (int i = 0; i < 1; i++) {
-            num.append(1 + random.nextInt(50));
+            num = (1 + random.nextInt(50));
         }
+//        System.out.println(num);
+//    }
+
+//    public void getPlayerGuess(int num) {
         System.out.println(num);
-    }
-
-    public String getPlayerGuess(){
         Scanner scan = new Scanner(System.in);
-        System.out.println("Please enter number to guess (from 1 to 50)");
-        String playerGuess = scan.next();
 
-        if (playerGuess.equals("exit") || playerGuess.equals("quit")){
-            System.exit(0);
+        while (true) {
+            System.out.println("Please enter number to guess (from 1 to 50)");
+            int playerGuess = scan.nextInt();
+
+            if (playerGuess == num) {
+                System.out.println("Congratulations! You win!");
+                break;
+            } else if (playerGuess <  num) {
+                System.out.println("Sorry! The number is higher...");
+            } else {
+                System.out.println("Sorry! The number is lower...");
+            }
         }
-        return playerGuess;
+//    }
+//    public static void main(String[] args){
+//        NumberGenerator gen = new NumberGenerator();
+//        gen.generateNumber();
+//        System.out.println("Hello!");
+//        NumberGenerator guess = new NumberGenerator();
+//        guess.getPlayerGuess(num);
+//    }
+//}
     }
-    public static void main(String[] args){
-        NumberGenerator gen = new NumberGenerator();
-        gen.generateNumber();
-        System.out.println("Hello!");
-        NumberGenerator guess = new NumberGenerator();
-        guess.getPlayerGuess();
-
-    }
-
 }
